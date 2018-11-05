be0a8a661eb521830f70b908735bc505d640a179
Derek <derekjohnbaker23@gmail.com>
2018-11-04 20:35:16 -0600

    Updated to include middleware

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+// use axios for API calls, jQeuery too bulky for simple calls
+// npm install --save axios
+
+const API_KEY = '';
+const ROOT_URL = '';
--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import reducers from './reducers';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware( reducers )}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
